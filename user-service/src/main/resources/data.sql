ddd9d134188d3a47ed3b746e820d5663ff033e7b
Ashav Kumar <35555091+ashavkumar@users.noreply.github.com>
2020-07-21 10:00:19 +0530

    Update data.sql

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO USER(USER_ID,FIRST_NAME,IS_ACTIVE,LAST_NAME,PASSWORD,ROLE,USER_NAME,MAIL_ID) 
-VALUES 	(1,'Ashav',true,'Kumar','Ashav11$','ROLE_ADMIN','ashakuma','ashav21011996@gmail.com'),
-	(2,'Neelam',true,'Topno','Neelam11$','ROLE_USER','neeltop','neelam28101997@gmail.com'),
-	(3,'Sushant',true,'Dey','Sushant11$','ROLE_USER','sushdey','sushant18091996@gmail.com'),
-	(4,'Somnath',true,'Dey','Somnath11$','ROLE_USER','somdey','somnath12111995@gmail.com'),
-	(5,'Shubham',true,'Arya','Shubham11$','ROLE_ADMIN','shubharya','shubham02011996@gmail.com');
+VALUES 	(1,'Ashav',true,'Kumar','Ashav11$','ROLE_ADMIN','ashakuma','xxxx6@gmail.com'),
+	(2,'Neelam',true,'Topno','Neelam11$','ROLE_USER','neeltop','xxxx7@gmail.com'),
+	(3,'Sushant',true,'Dey','Sushant11$','ROLE_USER','sushdey','xxxx6@gmail.com'),
+	(4,'Somnath',true,'Dey','Somnath11$','ROLE_USER','somdey','xxxx5@gmail.com'),
+	(5,'Shubham',true,'Arya','Shubham11$','ROLE_ADMIN','shubharya','xxxx6@gmail.com');
